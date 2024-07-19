cb3aa318d6280910dedbcf54bd40749752c5c1c8
Bhavesh Malviya <143591639+Bhavesh-malvi@users.noreply.github.com>
2024-07-19 20:02:00 +0530

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-export default clerkMiddleware({
-    publicRoutes: [
-        '/',
-        '/events/:id',
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ],
-    ignoredRoutes: [
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ]
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/events/:id',
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ],
+  ignoredRoutes: [
+    '/api/webhook/clerk',
+    '/api/webhook/stripe',
+    '/api/uploadthing'
+  ]
 });
-
+ 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+ 
